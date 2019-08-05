c9ff8af7d0871d19279936ee6bb0f2dbd1eb15f6
张威 <zw900808@gmail.com>
2019-08-05 10:41:56 +0800

    添加样式

--- src/Demo.js
+++ src/Demo.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { Button } from "antd";
+import "./demo.css";
 
 class Demo extends Component {
   render() {
     return (
-      <div>
+      <div className="demo-container">
+        <div className="demo-wrapper">this is my demo wrapper</div>
         <Button type="primary">this is demo component</Button>
       </div>
     );
